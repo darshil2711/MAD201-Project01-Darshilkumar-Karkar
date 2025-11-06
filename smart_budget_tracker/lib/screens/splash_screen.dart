e5a369a55d292ff0bd72218b9ac9718bd8c2909c
darshil2711 <darshilkarkar012@gmail.com>
2025-11-06 14:46:21 -0500

    feat: Add app skeleton and basic navigation

---
+++ smart_budget_tracker/lib/screens/splash_screen.dart
@@ -0,0 +1,43 @@
+/// MAD201-01 Assignment 3
+/// Darshilkumar Karkar - A00203357
+/// Splash Screen Implementation
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'home_screen.dart'; // Make sure to create this file
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 2),
+      () => Navigator.of(
+        context,
+      ).pushReplacement(MaterialPageRoute(builder: (context) => HomeScreen())),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.account_balance_wallet, size: 80),
+            SizedBox(height: 20),
+            Text(
+              "Smart Budget Tracker",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
